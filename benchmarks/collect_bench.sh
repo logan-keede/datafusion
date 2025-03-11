0fcf11b4dd16a68b4e5f0260d3d2932a49fe6a46
logan-keede <chawlakranti8@gmail.com>
2025-03-11 11:12:24 +0530

    still testing

--- benchmarks/collect_bench.sh
+++ benchmarks/collect_bench.sh
@@ -13,14 +13,15 @@ major_version=$(echo "$output" | grep -oE '[0-9]+' | head -n1)
 
 # run for current main
 echo "current major version: $major_version"  
-# ./bench.sh run $ARG1
+export RESULTS_DIR="results/main"
+./bench.sh run $ARG1
 
 # run for last 5 major releases
-for i in {3..5}; do
+for i in {1..5}; do
     echo "running benchmark on  $((major_version-i)).0.0"
     git fetch upstream $((major_version-i)).0.0
     git checkout $((major_version-i)).0.0
-    export RESULTS_DIR="results/heads_$((major_version-i)).0.0"
+    export RESULTS_DIR="results/$((major_version-i)).0.0"
     ./bench.sh run $ARG1
     # mv results/HEAD/* results/heads_$((major_version-i)).0.0/
 
